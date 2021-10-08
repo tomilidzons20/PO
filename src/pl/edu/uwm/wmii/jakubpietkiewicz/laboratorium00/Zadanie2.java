aaa779acbaa101aa0444f208c4607c9586cb0442
JP <jakubpietkiewicz@outlook.com>
2021-10-08 10:42:36 +0200

    testing1

---
+++ src/pl/edu/uwm/wmii/jakubpietkiewicz/laboratorium00/Zadanie2.java
@@ -0,0 +1,13 @@
+package pl.edu.uwm.wmii.jakubpietkiewicz.laboratorium00;
+
+public class Zadanie2 {
+    public static void main(String[] args)
+    {
+        int suma = 0;
+        for(int i = 0; i <= 10; i++)
+        {
+            suma += i;
+        }
+        System.out.println(suma);
+    }
+}
---
+++ src/pl/edu/uwm/wmii/jakubpietkiewicz/laboratorium00/Zadanie3.java
@@ -0,0 +1,13 @@
+package pl.edu.uwm.wmii.jakubpietkiewicz.laboratorium00;
+
+public class Zadanie3 {
+    public static void main(String[] args)
+    {
+        int iloczyn = 1;
+        for(int i = 1; i <= 10; i++)
+        {
+            iloczyn *= i;
+        }
+        System.out.println(iloczyn);
+    }
+}
